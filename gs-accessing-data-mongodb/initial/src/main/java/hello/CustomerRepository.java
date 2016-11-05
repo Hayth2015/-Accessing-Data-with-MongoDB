ebe05badfd974768635be038939161ef41925e67
Haytham <haytham.nasri@gmail.com>
2016-11-05 14:43:39 +0100

    Second commit

---
+++ gs-accessing-data-mongodb/initial/src/main/java/hello/CustomerRepository.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by admin on 05/11/16.
+ */
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
